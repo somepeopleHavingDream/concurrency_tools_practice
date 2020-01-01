0bcc00fd4a061b5ea0cb3a66c45fe8505502bdd0
yangxin <1393003255@qq.com>
2020-01-01 21:25:27 +0800

    feat: 五虎上将收复线程池

---
+++ src/main/java/threadpool/ShutDown.java
@@ -0,0 +1,42 @@
+package threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 演示关闭线程池
+ *
+ * @author yangxin
+ * 2020/01/01 21:13
+ */
+public class ShutDown {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 1000; i++) {
+            executorService.execute(new ShutDownTask());
+        }
+
+        TimeUnit.MILLISECONDS.sleep(1500);
+        executorService.shutdownNow();
+//        boolean b = executorService.awaitTermination(3L, TimeUnit.SECONDS);
+//        System.out.println(b);
+//        System.out.println(executorService.isShutdown());
+//        executorService.shutdown();
+//        System.out.println(executorService.isShutdown());
+//        System.out.println(executorService.isTerminated());
+//        executorService.execute(new ShutDownTask());
+    }
+}
+
+class ShutDownTask implements Runnable {
+    @Override
+    public void run() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(500);
+            System.out.println(Thread.currentThread().getName());
+        } catch (InterruptedException e) {
+            System.out.println(Thread.currentThread().getName() + "被中断了");
+        }
+    }
+}
