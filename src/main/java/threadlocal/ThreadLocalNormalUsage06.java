afc2bae8e099d73d12de5c549241603050b0ceac
yangxin <1393003255@qq.com>
2020-01-01 18:57:26 +0800

    feat: 收不回来的value

--- src/main/java/threadlocal/ThreadLocalNormalUsage06.java
+++ src/main/java/threadlocal/ThreadLocalNormalUsage06.java
@@ -34,6 +34,7 @@ class Service3 {
     void process() {
         User user = UserContextHolder.holder.get();
         System.out.println("Service3拿到用户名： " + user.name);
+        UserContextHolder.holder.remove();
     }
 }
 
