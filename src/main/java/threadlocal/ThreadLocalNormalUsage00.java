8d0c38d61c6bac066990b5e20cb2c5e024d96d1b
yangxin <1393003255@qq.com>
2020-01-01 17:12:44 +0800

    feat: ThreadLocal的两种用法

---
+++ src/main/java/threadlocal/ThreadLocalNormalUsage00.java
@@ -0,0 +1,31 @@
+package threadlocal;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 两个线程打印日期
+ *
+ * @author yangxin
+ * 2020/01/01 16:54
+ */
+public class ThreadLocalNormalUsage00 {
+    private String date(int seconds) {
+        // 参数的单位是毫秒，从1970/01/01 00:00:00 gmt计时
+        Date date = new Date(1000 * seconds);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        return simpleDateFormat.format(date);
+    }
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            String date = new ThreadLocalNormalUsage00().date(10);
+            System.out.println(date);
+        }).start();
+
+        new Thread(() -> {
+            String date = new ThreadLocalNormalUsage00().date(1007);
+            System.out.println(date);
+        }).start();
+    }
+}
