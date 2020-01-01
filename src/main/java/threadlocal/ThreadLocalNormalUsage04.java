2dc5f7867e2a5de95ea1f0494f74e21fe107ed0f
yangxin <1393003255@qq.com>
2020-01-01 17:53:09 +0800

    feat: 走过的弯路，你的套路

---
+++ src/main/java/threadlocal/ThreadLocalNormalUsage04.java
@@ -0,0 +1,40 @@
+package threadlocal;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 1000个线程打印日期的任务，用线程池来执行，SimpleDateFormat对象唯一，加锁来解决问题
+ *
+ * @author yangxin
+ * 2020/01/01 17:29
+ */
+public class ThreadLocalNormalUsage04 {
+    private static ExecutorService threadPool = Executors.newFixedThreadPool(10);
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+
+    private String date(int seconds) {
+        // 参数的单位是毫秒，从1970/01/01 00:00:00 gmt计时
+        Date date = new Date(1000 * seconds);
+        String format;
+        synchronized (ThreadLocalNormalUsage04.class) {
+            format = simpleDateFormat.format(date);
+        }
+
+        return format;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            int finalI = i;
+            threadPool.submit(() -> {
+                String date = new ThreadLocalNormalUsage04().date(finalI);
+                System.out.println(date);
+            });
+        }
+        threadPool.shutdown();
+    }
+}
