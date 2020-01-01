0e25a70f756f6599e1e7c3cf741faff1d352544d
yangxin <1393003255@qq.com>
2020-01-01 20:11:13 +0800

    feat: 线程池的自我介绍

---
+++ src/main/java/threadpool/EveryTaskOneThread.java
@@ -0,0 +1,21 @@
+package threadpool;
+
+/**
+ * 每个任务一个线程
+ *
+ * @author yangxin
+ * 2020/01/01 20:05
+ */
+public class EveryTaskOneThread {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Task());
+        thread.start();
+    }
+
+    static class Task implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("执行了任务");
+        }
+    }
+}
---
+++ src/main/java/threadpool/ForLoop.java
@@ -0,0 +1,23 @@
+package threadpool;
+
+/**
+ * 每个任务一个线程
+ *
+ * @author yangxin
+ * 2020/01/01 20:05
+ */
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            Thread thread = new Thread(new Task());
+            thread.start();
+        }
+    }
+
+    static class Task implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("执行了任务");
+        }
+    }
+}
