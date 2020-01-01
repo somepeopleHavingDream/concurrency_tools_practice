533bcb38fadd8a95b8eab097e9837ab5cc2fc511
yangxin <1393003255@qq.com>
2020-01-01 20:53:54 +0800

    feat: 演示内存溢出的情况

---
+++ src/main/java/threadpool/FixedThreadPoolOOM.java
@@ -0,0 +1,35 @@
+package threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 演示newFixedThreadPool出错的情况
+ *
+ * @author yangxin
+ * 2020/01/01 20:37
+ */
+public class FixedThreadPoolOOM {
+    private static ExecutorService executorService = Executors.newFixedThreadPool(1);
+
+    /**
+     * -Xmx8m -Xms8m
+     */
+    public static void main(String[] args) {
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            executorService.execute(new SubThread());
+        }
+    }
+}
+
+class SubThread implements Runnable {
+    @Override
+    public void run() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/threadpool/FixedThreadPoolTest.java
@@ -0,0 +1,33 @@
+package threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 演示newFixedThreadPool
+ *
+ * @author yangxin
+ * 2020/01/01 20:31
+ */
+public class FixedThreadPoolTest {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        for (int i = 0; i < 1000; i++) {
+            executorService.execute(new Task());
+        }
+    }
+}
+
+class Task implements Runnable {
+    @Override
+    public void run() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(Thread.currentThread().getName());
+    }
+}
