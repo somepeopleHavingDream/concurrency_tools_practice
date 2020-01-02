2b9af45a39305836f7e3aef3e8f948d7ad4d9c23
yangxin <1393003255@qq.com>
2020-01-02 18:04:35 +0800

    feat: 把普通变量升级为原子变量

---
+++ src/main/java/atomic/AtomicIntegerFieldUpdaterDemo.java
@@ -0,0 +1,45 @@
+package atomic;
+
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+/**
+ * 演示AtomicIntegerFieldUpdater的用法
+ *
+ * @author yangxin
+ * 2020/01/02 16:44
+ */
+public class AtomicIntegerFieldUpdaterDemo implements Runnable {
+    private static Candidate tom;
+    private static Candidate peter;
+
+    private static final AtomicIntegerFieldUpdater<Candidate> scoreUpdater = AtomicIntegerFieldUpdater
+            .newUpdater(Candidate.class, "score");
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            peter.score++;
+            scoreUpdater.getAndIncrement(tom);
+        }
+    }
+
+    private static class Candidate {
+        volatile int score;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        tom = new Candidate();
+        peter = new Candidate();
+
+        AtomicIntegerFieldUpdaterDemo atomicIntegerFieldUpdaterDemo = new AtomicIntegerFieldUpdaterDemo();
+        Thread t1 = new Thread(atomicIntegerFieldUpdaterDemo);
+        Thread t2 = new Thread(atomicIntegerFieldUpdaterDemo);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println("普通变量：" + peter.score);
+        System.out.println("升级后的结果：" + tom.score);
+    }
+}
