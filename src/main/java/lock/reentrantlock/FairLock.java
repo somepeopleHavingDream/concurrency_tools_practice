8c2b7fe0da451acf0ff602c3d3faeb9fcd0f66b3
yangxin <1393003255@qq.com>
2020-02-13 20:26:00 +0800

    feat: 先来后到的特例、优劣、源码分析

---
+++ src/main/java/lock/reentrantlock/FairLock.java
@@ -0,0 +1,74 @@
+package lock.reentrantlock;
+
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 演示公平和不公平两种情况
+ *
+ * @author yangxin
+ * 2020/02/13 20:04
+ */
+public class FairLock {
+    public static void main(String[] args) {
+        PrintQueue printQueue = new PrintQueue();
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < 10; i++) {
+            threads[i] = new Thread(new Job(printQueue));
+        }
+
+        for (int i = 0; i < 10; i++) {
+            threads[i].start();
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Job implements Runnable {
+    private PrintQueue printQueue;
+
+    Job(PrintQueue printQueue) {
+        this.printQueue = printQueue;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "开始打印");
+        printQueue.printJob(new Object());
+        System.out.println(Thread.currentThread().getName() + "打印完毕");
+    }
+}
+
+class PrintQueue {
+    private Lock queueLock = new ReentrantLock(false);
+//    private Lock queueLock = new ReentrantLock(true);
+
+    void printJob(Object document) {
+        queueLock.lock();
+        try {
+            int duration = new Random().nextInt(10) + 1;
+            System.out.println(Thread.currentThread().getName() + "正在打印，需要" + duration);
+            Thread.sleep(duration * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            queueLock.unlock();
+        }
+
+        queueLock.lock();
+        try {
+            int duration = new Random().nextInt(10) + 1;
+            System.out.println(Thread.currentThread().getName() + "正在打印，需要" + duration);
+            Thread.sleep(duration * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            queueLock.unlock();
+        }
+    }
+}
