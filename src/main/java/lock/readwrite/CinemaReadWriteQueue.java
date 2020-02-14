3581d0e9941d667fa39855054cbf9d7fe9adfe90
yangxin <1393003255@qq.com>
2020-02-14 17:42:39 +0800

    feat: 读写锁的由奢入俭“易”

---
+++ src/main/java/lock/readwrite/CinemaReadWriteQueue.java
@@ -0,0 +1,47 @@
+package lock.readwrite;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author yangxin
+ * 2020/02/14 17:23
+ */
+public class CinemaReadWriteQueue {
+    private static ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock(false);
+    private static ReentrantReadWriteLock.ReadLock readLock = reentrantReadWriteLock.readLock();
+    private static ReentrantReadWriteLock.WriteLock writeLock = reentrantReadWriteLock.writeLock();
+
+    private static void read() {
+        readLock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "得到了读锁，正在读取");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(Thread.currentThread().getName() + "释放读锁");
+            readLock.unlock();
+        }
+    }
+
+    private static void write() {
+        writeLock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "得到了写锁，正在写入");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(Thread.currentThread().getName() + "释放写锁");
+            writeLock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(CinemaReadWriteQueue::write, "Thread1").start();
+        new Thread(CinemaReadWriteQueue::read, "Thread2").start();
+        new Thread(CinemaReadWriteQueue::read, "Thread3").start();
+        new Thread(CinemaReadWriteQueue::write, "Thread4").start();
+        new Thread(CinemaReadWriteQueue::read, "Thread5").start();
+    }
+}
