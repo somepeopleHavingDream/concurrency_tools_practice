e5dd34a0786a3029b93a1c056d0843c772f14e85
yangxin <1393003255@qq.com>
2020-02-15 21:38:33 +0800

    feat: 什么是“不变性”

---
+++ src/main/java/immutable/TestFinal.java
@@ -0,0 +1,14 @@
+package immutable;
+
+/**
+ * 测试final能否被修改
+ *
+ * @author yangxin
+ * 2020/02/15 21:36
+ */
+public class TestFinal {
+    public static void main(String[] args) {
+        Person person = new Person();
+//        person.age = 19;
+    }
+}
