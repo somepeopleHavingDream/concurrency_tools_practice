99093de53eee247e060fb6e17963f40fa31e094d
yangxin <1393003255@qq.com>
2020-02-18 16:33:24 +0800

    feat: 彩蛋：调整JDK版本，以便查看对应版本的代码

--- hashmapendlessloop/src/HashMapEndlessLoop.java
+++ hashmapendlessloop/src/HashMapEndlessLoop.java
@@ -1,9 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author yangxin
  * 2020/02/17 22:00
  */
 public class HashMapEndlessLoop {
     public static void main(String[] args) {
-
+        Map<String, Object> map = new HashMap<>();
     }
 }
