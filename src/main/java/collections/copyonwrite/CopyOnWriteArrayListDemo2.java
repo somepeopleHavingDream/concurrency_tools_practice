93c34332dd852f655f2e12a75efa902f7132ad46
yangxin <1393003255@qq.com>
2020-02-19 21:04:01 +0800

    feat: CopyOnWrite设计理念、数据过期问题

---
+++ src/main/java/collections/copyonwrite/CopyOnWriteArrayListDemo2.java
@@ -0,0 +1,31 @@
+package collections.copyonwrite;
+
+import com.sun.xml.internal.ws.addressing.WsaActionUtil;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * 对比两个迭代器
+ *
+ * @author yangxin
+ * 2020/02/19 20:57
+ */
+public class CopyOnWriteArrayListDemo2 {
+    public static void main(String[] args) {
+        List<Integer> list = new CopyOnWriteArrayList<>(new Integer[]{1, 2, 3});
+        System.out.println(list);
+
+        Iterator<Integer> iterator1 = list.iterator();
+        list.add(4);
+        System.out.println(list);
+
+        Iterator<Integer> iterator2 = list.iterator();
+
+        iterator1.forEachRemaining(System.out::println);
+        System.out.println();
+        iterator2.forEachRemaining(System.out::println);
+    }
+}
