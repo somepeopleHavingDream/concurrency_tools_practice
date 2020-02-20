a81ba3dc195e166126bdd514b47f9ba0cab017ef
yangxin <1393003255@qq.com>
2020-02-20 20:40:27 +0800

    feat: 循环栅栏的作用

---
+++ src/main/java/flowcontrol/cyclicbarrier/CyclicBarrierDemo.java
@@ -0,0 +1,47 @@
+package flowcontrol.cyclicbarrier;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * 演示CyclicBarrier
+ *
+ * @author yangxin
+ * 2020/02/20 20:24
+ */
+public class CyclicBarrierDemo {
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(5, CyclicBarrierDemo::run);
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Task(i, cyclicBarrier)).start();
+        }
+    }
+
+    private static void run() {
+        System.out.println("所有人都到场，大家统一出发！");
+    }
+
+    static class Task implements Runnable {
+        private int id;
+        private CyclicBarrier cyclicBarrier;
+
+        Task(int id, CyclicBarrier cyclicBarrier) {
+            this.id = id;
+            this.cyclicBarrier = cyclicBarrier;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("线程" + id + "现在前往集合地点");
+            try {
+                Thread.sleep((long) (Math.random() * 10000));
+                System.out.println("线程" + id + "到了集合地点，开始等待其他人到达");
+                cyclicBarrier.await();
+                System.out.println("线程" + id + "出发了");
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
