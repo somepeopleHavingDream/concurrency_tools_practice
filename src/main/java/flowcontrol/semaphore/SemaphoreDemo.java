7169d7db52ef80dab6f633def6abdc7b6bec537a
yangxin <1393003255@qq.com>
2020-02-20 14:28:54 +0800

    feat: Semaphore用法和注意点

---
+++ src/main/java/flowcontrol/semaphore/SemaphoreDemo.java
@@ -0,0 +1,46 @@
+package flowcontrol.semaphore;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * 演示Semaphore的用法
+ *
+ * @author yangxin
+ * 2020/02/20 14:18
+ */
+public class SemaphoreDemo {
+    private static Semaphore semaphore = new Semaphore(3, true);
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(50);
+        for (int i = 0; i < 100; i++) {
+            executorService.submit(new Task());
+        }
+        executorService.shutdown();
+    }
+
+    static class Task implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                semaphore.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(Thread.currentThread().getName() + "拿到了许可证");
+
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(Thread.currentThread().getName() + "释放了许可证");
+            semaphore.release();
+        }
+    }
+}
