5bccf10bf57dfee025cce30e81d083bfabbc3443
yangxin <1393003255@qq.com>
2020-02-20 20:50:11 +0800

    feat: Semaphore和AQS的关系

---
+++ src/main/java/aqs/AQSDemo.java
@@ -0,0 +1,13 @@
+package aqs;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author yangxin
+ * 2020/02/20 20:48
+ */
+public class AQSDemo {
+    public static void main(String[] args) {
+        new Semaphore(5);
+    }
+}
