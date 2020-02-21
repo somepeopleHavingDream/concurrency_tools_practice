7813c46eb970be53b264eb4b524ce3d070a7dbf5
yangxin <1393003255@qq.com>
2020-02-21 22:44:33 +0800

    feat: 用FutureTask获取结果

---
+++ src/main/java/future/FutureTaskDemo.java
@@ -0,0 +1,40 @@
+package future;
+
+import java.util.concurrent.*;
+
+/**
+ * 演示FutureTask的用法
+ *
+ * @author yangxin
+ * 2020/02/21 22:33
+ */
+public class FutureTaskDemo {
+    public static void main(String[] args) {
+        Task task = new Task();
+        FutureTask<Integer> futureTask = new FutureTask<>(task);
+//        new Thread(futureTask).start();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.submit(futureTask);
+
+        try {
+            System.out.println("task运行结果：" + futureTask.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Task implements Callable<Integer> {
+
+    @Override
+    public Integer call() throws InterruptedException {
+        System.out.println("子线程正在计算");
+        Thread.sleep(3000);
+
+        int sum = 0;
+        for (int i = 0; i <= 100; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
