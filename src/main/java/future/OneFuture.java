aa5a28a9ef186a7c37bbc618602adb6b1092b224
yangxin <1393003255@qq.com>
2020-02-21 21:13:18 +0800

    feat: submit返回Future对象，快速抽取返回值

---
+++ src/main/java/future/OneFuture.java
@@ -0,0 +1,36 @@
+package future;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+/**
+ * 演示一个Future的使用方法
+ *
+ * @author yangxin
+ * 2020/02/021 20:55
+ */
+public class OneFuture {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        Future<Integer> future = executorService.submit(new CallableTask());
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        executorService.shutdown();
+    }
+
+    /**
+     * @author yangxin
+     * 2020/02/21 20:57
+     */
+    static class CallableTask implements Callable<Integer> {
+
+        @Override
+        public Integer call() throws InterruptedException {
+            Thread.sleep(3000);
+            return new Random().nextInt();
+        }
+    }
+}
