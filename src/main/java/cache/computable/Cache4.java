d2ec5f72397fdeb99556e3eea1fc1fb659d2b361
yangxin <1393003255@qq.com>
2020-02-22 12:03:36 +0800

    feat: 用ConcurrentHashMap保证并发安全

---
+++ src/main/java/cache/computable/Cache4.java
@@ -0,0 +1,41 @@
+package cache.computable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 缩小了synchronized额粒度，提高性能，但是依然并发不安全
+ *
+ * @author yangxin
+ * 2020/02/22 11:36
+ */
+public class Cache4<T, R> implements Computable<T, R> {
+    private final Map<T, R> cache = new HashMap<>();
+    private final Computable<T, R> computable;
+
+    private Cache4(Computable<T, R> computable) {
+        this.computable = computable;
+    }
+
+    @Override
+    public R compute(T arg) throws Exception {
+        System.out.println("进入缓存机制");
+        R result = cache.get(arg);
+        if (result == null) {
+            result = computable.compute(arg);
+            synchronized (this) {
+                cache.put(arg, result);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Computable<String, Integer> expensiveComputer = new Cache4<>(new ExpensiveFunction());
+        Integer result = expensiveComputer.compute("666");
+        System.out.println("第一次计算结果：" + result);
+        result = expensiveComputer.compute("666");
+        System.out.println("第二次计算结果：" + result);
+    }
+}
---
+++ src/main/java/cache/computable/Cache5.java
@@ -0,0 +1,39 @@
+package cache.computable;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 缩小了synchronized额粒度，提高性能，但是依然并发不安全
+ *
+ * @author yangxin
+ * 2020/02/22 11:36
+ */
+public class Cache5<T, R> implements Computable<T, R> {
+    private final Map<T, R> cache = new ConcurrentHashMap<>();
+    private final Computable<T, R> computable;
+
+    private Cache5(Computable<T, R> computable) {
+        this.computable = computable;
+    }
+
+    @Override
+    public R compute(T arg) throws Exception {
+        System.out.println("进入缓存机制");
+        R result = cache.get(arg);
+        if (result == null) {
+            result = computable.compute(arg);
+            cache.put(arg, result);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Computable<String, Integer> expensiveComputer = new Cache5<>(new ExpensiveFunction());
+        Integer result = expensiveComputer.compute("666");
+        System.out.println("第一次计算结果：" + result);
+        result = expensiveComputer.compute("666");
+        System.out.println("第二次计算结果：" + result);
+    }
+}
