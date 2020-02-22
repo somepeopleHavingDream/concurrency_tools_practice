5e1e2511a45002656719de6460465094de6aa0ec
yangxin <1393003255@qq.com>
2020-02-22 22:03:08 +0800

    feat: 用CountDownLatch实现压测

--- src/main/java/cache/Cache12.java
+++ src/main/java/cache/Cache12.java
@@ -3,6 +3,7 @@ package cache;
 import cache.computable.Computable;
 import cache.computable.ExpensiveFunction;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -12,15 +13,19 @@ import java.util.concurrent.Executors;
  */
 public class Cache12 {
     private static Computable<String, Integer> computable = new Cache10<>(new ExpensiveFunction());
+    private static CountDownLatch countDownLatch = new CountDownLatch(1);
 
-    public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(3000);
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
 
         long start = System.currentTimeMillis();
-        for (int i = 0; i < 3000; i++) {
+        for (int i = 0; i < 100; i++) {
             executorService.submit(() -> {
                 Integer result = null;
                 try {
+                    System.out.println(Thread.currentThread().getName() + "开始等待");
+                    countDownLatch.await();
+                    System.out.println(Thread.currentThread().getName() + "被放行");
                     result = computable.compute("666");
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -29,10 +34,14 @@ public class Cache12 {
             });
         }
 
+        Thread.sleep(5000);
+        countDownLatch.countDown();
         executorService.shutdown();
-        while (!executorService.isTerminated()) {
 
-        }
-        System.out.println("总耗时：" + (System.currentTimeMillis() - start));
+//        executorService.shutdown();
+//        while (!executorService.isTerminated()) {
+//
+//        }
+//        System.out.println("总耗时：" + (System.currentTimeMillis() - start));
     }
 }
