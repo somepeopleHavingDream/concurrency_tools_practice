5720a2a3fa2cf98d96a5f6674b5b2410e539a1ff
yangxin <1393003255@qq.com>
2020-07-27 17:23:20 +0800

    style: 前身：同步的HashMap和ArrayList

--- src/main/java/collections/predecessor/SynList.java
+++ src/main/java/collections/predecessor/SynList.java
@@ -11,6 +11,7 @@ import java.util.List;
  * 2020/02/17 21:41
  */
 public class SynList {
+
     public static void main(String[] args) {
         List<Integer> list= Collections.synchronizedList(new ArrayList<>());
         list.add(5);
